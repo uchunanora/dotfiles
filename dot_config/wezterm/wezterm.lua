721784b38bb6c8308528f83026f67ec41fb0216a
uchunanora <uchusawai@gmail.com>
2026-04-17 13:29:13 +0900

    add WezTerm and Yazi initial configs

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = "Tokyo Night"
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 20
+-- config.window_decorations = "RESIZE"
+
+-- and finally, return the configuration to wezterm
+return config
